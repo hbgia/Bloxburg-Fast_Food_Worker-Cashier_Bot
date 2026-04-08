f6860703f6c79d0fb313b836f9cae920d63f429b
Jacky <noname132006@gmail.com>
2026-04-08 14:20:05 +0700

    edited just for more beautiful code

--- serving_menu_offset.py
+++ serving_menu_offset.py
@@ -2,8 +2,8 @@ main_dish_GUI_offset = {
     15: [0.5, 0.92],    # BottomBun
     6:  [0.5, 0.8],     # ORDER_Lettuce
     13: [0.5, 0.64],    # ORDER_Tomato
-    12: [0.35, 0.5],     # ORDER_Patty
-    14: [0.65, 0.5],     # ORDER_VeganPatty
+    12: [0.35, 0.5],    # ORDER_Patty
+    14: [0.65, 0.5],    # ORDER_VeganPatty
     2:  [0.5, 0.37],    # ORDER_Cheese
     9:  [0.5, 0.23],    # ORDER_Onion
     16: [0.5, 0.08]     # TopBun
@@ -16,9 +16,9 @@ side_dish_GUI_offset = {
     3:  [0.25, 0.25],   # ORDER_FountainDrink
     5:  [0.25, 0.5],    # ORDER_FruityFruitJuice
     7:  [0.25, 0.75],   # ORDER_MilkShake
-    'S': [0.75, 0.25],   # Size S
-    'M': [0.75, 0.5],    # Size M
-    'L': [0.75, 0.75],   # Size L
+    'S': [0.75, 0.25],  # Size S
+    'M': [0.75, 0.5],   # Size M
+    'L': [0.75, 0.75],  # Size L
 }
 
 # how to call: print(main_dish_GUI_offset[15])  # output [0.5, 0.92]
